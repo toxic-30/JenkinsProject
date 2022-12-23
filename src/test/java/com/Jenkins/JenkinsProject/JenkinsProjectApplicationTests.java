313a396a3fb5082d6a478823bc99e869afd87fb8
toxic-30 <shivangipandey556@gmail.com>
2022-12-23 23:19:50 +0530

    first commit

---
+++ src/test/java/com/Jenkins/JenkinsProject/JenkinsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Jenkins.JenkinsProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
