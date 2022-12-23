01b71e34db02cf74168c3c302b08551778ecccc1
toxic-30 <73540247+toxic-30@users.noreply.github.com>
2022-12-24 00:01:08 +0530

    Update JenkinsProjectApplication.java

--- src/main/java/com/Jenkins/JenkinsProject/JenkinsProjectApplication.java
+++ src/main/java/com/Jenkins/JenkinsProject/JenkinsProjectApplication.java
@@ -8,6 +8,7 @@ public class JenkinsProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsProjectApplication.class, args);
+		System.out.println("Happy World");
 	}
 
 }
